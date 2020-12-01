ef0a9c73444ac593d53e57b4cfcb75307f9edd6b
Murat Cengiz <mcengiz240296@gmail.com>
2020-12-01 16:25:09 +0100

    Fix SQL init

--- src/server/database/init.sql
+++ src/server/database/init.sql
@@ -1,7 +1,7 @@
-CREATE TABLE [IF NOT EXISTS] components_cache (
+CREATE TABLE IF NOT EXISTS components_cache (
    component varchar,
    metrics varchar,
    company varchar,
    period varchar,
-   value integer,
+   value integer
 );
