b5032e985f080bc2949e6112d8693fd016feab85
Murat Cengiz <mcengiz240296@gmail.com>
2020-12-14 12:21:30 +0100

    Local cache and save to database

--- src/server/database/index.js
+++ src/server/database/index.js
@@ -5,22 +5,26 @@ const { Client } = require('pg')
 let client
 
 async function init(clientConfig) {
-  client = new Client(clientConfig)
-  await client.connect()
+  try {
+    client = new Client(clientConfig)
+    await client.connect()
+  } catch (e) {
+    client = undefined
+    throw e
+  }
 }
 
 async function createTables() {
   const filePath = 'init.sql'
   const sql = fs.readFileSync(path.join(__dirname, filePath), { encoding: 'utf8' })
-  return await client.query(sql);
+  if (client) return await client.query(sql);
 }
 
 async function selectFrom(table, columns, where) {
   const sql = `SELECT ${columns.join(', ')} \n` +
     `FROM ${table} \n` +
     `WHERE ${where.join(' AND \n')};`
-  console.log(sql)
-  return await client.query(sql)
+  if (client) return await client.query(sql)
 }
 
 async function insertInto(table, columns = [], rows = []) {
@@ -28,8 +32,23 @@ async function insertInto(table, columns = [], rows = []) {
     'VALUES \n' +
     `${rows.map(row => `(${row.join(', ')})`).join(',\n')} \n` +
     'RETURNING *;'
-  console.log(sql)
-  return await client.query(sql)
+  if (client) return await client.query(sql)
+}
+
+async function replaceInto(table, columns = [], rows = []) {
+  const sql = `REPLACE INTO ${table} (${columns.join(', ')}) \n` +
+    'VALUES \n' +
+    `${rows.map(row => `(${row.join(', ')})`).join(',\n')} \n` +
+    'RETURNING *;'
+  if (client) return await client.query(sql)
+}
+
+async function update(table, values = {}, conditions = []) {
+  const sql = `UPDATE ${table} \n` +
+    `SET ${Object.entries(values).map(entry => `${entry[0]} = ${entry[1]}`).join(',\n')} \n` +
+    `WHERE ${conditions.join(' AND ')} \n` +
+    'RETURNING *;'
+  if (client) return await client.query(sql)
 }
 
 module.exports = {
@@ -37,4 +56,6 @@ module.exports = {
   createTables,
   selectFrom,
   insertInto,
+  replaceInto,
+  update,
 }
--- src/server/utils.js
+++ src/server/utils.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const HTMLParser = require('node-html-parser');
 
+const database = require('./database');
+
 let componentsCache;
 
 const hostname = 'devstats.cncf.io';
@@ -27,27 +29,158 @@ function queryToBody(query) {
 
 async function loadCompanies(component) {
   // retrieve companies list
-  const query = "select name from \"shcom\" where series = 'hcomcontributions' and period = 'y10'"
-  return await fetchData(component, query)
+  const response = await loadData(component, 'hcomcontributions', [ 'y10' ])
+  return response.data.rows.map(company => company.name)
+}
+
+const localCache = {}
+
+function shouldUpdateCache(cachedData, companies) {
+  // no cache
+  if (!cachedData) {
+    return true
+  }
+
+  // missing company info in cache
+  if (companies) {
+    for (const company of companies) {
+      if (cachedData.missing.indexOf(company) !== -1) {
+        return true
+      }
+    }
+  }
+
+  return false
 }
 
 async function loadData(component, metrics, periods, companies) {
-  const query = `select name, value, period from \"shcom\" where series = '${metrics}' and period in (${periods.map(p => `'${p}'`).join(', ')})`
-  const response = await fetchData(component, query)
-  response.data = processData(response.data, companies)
+  const response = {}
+
+  const cachedData = loadFromCache(component, metrics, periods)
+  if (shouldUpdateCache(cachedData, companies)) {
+    response.data = await loadFromDevstats(component, metrics, periods)
+    const rowsToAdd = saveToLocalCache(component, metrics, response.data)
+    if (rowsToAdd.length > 0) {
+      await saveToDatabase(rowsToAdd)
+    }
+  } else {
+    response.data = cachedData
+  }
   //response.data.sort() // Sort alphabetically
 
+  if (companies) {
+    response.data.rows = response.data.rows.filter(company => companies.indexOf(company.name) !== -1)
+  }
   return response
+}
+
+function loadFromCache(component, metrics, periods) {
+  const componentCache = localCache[component]
+  if (componentCache) {
+    const metricsCache = componentCache[metrics]
+    if (metricsCache) {
+      const missingCompanies = []
+      const rows = []
+      for (const entry of Object.entries(metricsCache)) {
+        let missing = false
+        const row = {}
+        const company = entry[0]
+        const values = entry[1]
+        row.name = company
+        for (const p of periods) {
+          const value = values[p]
+          if (value !== undefined) {
+            row[p] = value
+          } else {
+            missingCompanies.push(company)
+            missing = true
+            break
+          }
+        }
+        if (missing) continue
+
+        rows.push(row)
+      }
+
+      const data = {}
+
+      data.columns = [ 'name' ].concat(periods)
+      data.rows = rows
+      data.missing = missingCompanies
+
+      return data
+    }
+  }
+}
+
+async function loadFromDevstats(component, metrics, periods) {
+  let query = `select name, value, period from \"shcom\" where series = '${metrics}' and period `
+  if (periods.length > 1) {
+    query += `in (${periods.map(p => `'${p}'`).join(', ')})`
+  } else {
+    query += `= '${periods[0]}'`
+  }
+  const response = await fetchData(component, query)
+  return processData(response.data)
+}
+
+function saveToLocalCache(component, metrics, data) {
+  let componentCache = localCache[component]
+  if (!componentCache) {
+    componentCache = {}
+    localCache[component] = componentCache
+  }
+  let metricsCache = componentCache[metrics]
+  if (!metricsCache) {
+    metricsCache = {}
+    componentCache[metrics] = metricsCache
+  }
+
+  const mainColumn = data.columns[0]
+  const columns = data.columns.slice(1)
+  const rowsToAdd = []
+  for (const row of data.rows) {
+    const company = row[mainColumn]
+    let companyCache = metricsCache[company]
+    if (!companyCache) {
+      companyCache = {}
+      metricsCache[company] = companyCache
+    }
+
+    const rowToAdd = [component, metrics, company]
+    for (const period of columns) {
+      const value = row[period] || 0
+      rowsToAdd.push(rowToAdd.concat([period, value]))
+      companyCache[period] = value
+    }
+  }
+
+  return rowsToAdd
+}
+
+async function saveToDatabase(data) {
+  return await database.replaceInto(
+    'components_cache',
+    [
+      'component',
+      'metrics',
+      'company',
+      'period',
+      'value',
+    ],
+    data,
+  )
 }
 
 async function fetchData(component, query) {
   const body = queryToBody(query)
 
   const url = `https://${component}.${hostname}/api/tsdb/query`
+  console.log(`${url} : ${query}`)
   return await axios.post(url, body)
 }
 
-function processData(data, companies) {
+function processData(data) {
   const table = data.results['A'].tables[0]
   table.columns = table.columns.map(obj => obj.text)
 
@@ -65,18 +198,14 @@ function processData(data, companies) {
 
     const value = row[1]
     const name = row[2]
-    keys.indexOf(name) === -1 && keys.push(name)
-    obj[name] = value
+    if (name && value) {
+      keys.indexOf(name) === -1 && keys.push(name)
+      obj[name] = value
+    }
   })
 
   const out = {}
-  const rows = []
-  for (const key in dictValues) {
-    if (companies.indexOf(key) !== -1) {
-      rows.push(dictValues[key])
-    }
-  }
-  out.rows = rows
+  out.rows = Object.values(dictValues)
   out.columns = keys
 
   return out
