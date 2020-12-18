34187ddc72a5b6c662985fc7700f6fc56084b875
Murat Cengiz <mcengiz2402@gmail.com>
2020-12-18 16:36:36 +0100

    Completed component stack management (UI part)

--- src/public/js/stack.js
+++ src/public/js/stack.js
@@ -3,10 +3,70 @@ const apiBaseUrl = window.location.href
 let tabindex = 1;
 
 // TODO : for testing purpose
-const components = [ 'Kubernetes', 'Docker', 'Helm' ];
-const componentStacks = [ 'Stack1', 'Stack2', 'Stack3' ];
-const companies = [ 'Google', 'Amazon', 'Microsoft' ];
-const companyStacks = [ 'Group1', 'Group2', 'Group3' ];
+const components = [
+  {
+    short: 'k8s',
+    name: 'Kubernetes',
+    href: '',
+  },
+  {
+    short: 'docker',
+    name: 'Docker',
+    href: '',
+  },
+  {
+    short: 'helm',
+    name: 'Helm',
+    href: '',
+  },
+];
+const componentStacks = [
+  {
+    name: 'Stack 1',
+    components: components,
+  },
+  {
+    name: 'Stack 2',
+    components: components.slice(1),
+  },
+  {
+    name: 'Stack 3',
+    components: components.slice(2),
+  },
+];
+const companies = [
+  {
+    name: 'Google',
+  },
+  {
+    name: 'Microsoft',
+  },
+  {
+    name: 'Amazon',
+  },
+];
+const companyStacks = [
+  {
+    name: 'Group 1',
+    components: companies,
+  },
+  {
+    name: 'Group 2',
+    components: companies.slice(1),
+  },
+  {
+    name: 'Group 3',
+    components: companies.slice(2),
+  },
+];
+
+async function loadComponents() {
+  return components;
+}
+
+async function loadComponentStacks() {
+  return componentStacks;
+}
 
 function appendElement(parent, tagName, attributes = {}) {
   const element = document.createElement(tagName);
@@ -56,7 +116,11 @@ function createSubGrid(parent, textLabel) {
   return subGrid;
 }
 
-function createSelection(parent, id, callback, multiple, placeholder) {
+function getSelectedItems(selection) {
+  return Array.from(selection.listElements).filter(element => element.className.indexOf('active') !== -1).map(element => element.getAttribute('data-value'));
+}
+
+function createSelection(parent, id, callback, multiple, placeholder, disabled = false) {
   const selectAttributes = {
     id: id,
   };
@@ -77,18 +141,27 @@ function createSelection(parent, id, callback, multiple, placeholder) {
   let multipleSelection = new vanillaSelectBox(`#${id}`, selectionOptions);
   const pointer = {};
   callback().then(function (values) {
+    multipleSelection = pointer.selection;
+    const listener = multipleSelection.main.lastElementChild.onclick;
     for (const value of values) {
       const option = document.createElement('option');
-      option.setAttribute('value', value);
-      option.innerHTML = value;
+      option.setAttribute('value', value.short || value.name);
+      option.innerHTML = value.name;
       select.append(option);
     }
     multipleSelection.destroy();
 
-    delete selectionOptions.placeHolder;
+    if (!disabled) delete selectionOptions.placeHolder;
     const localTabindex = multipleSelection.main.getAttribute('tabindex');
     multipleSelection = new vanillaSelectBox(`#${id}`, selectionOptions);
+    multipleSelection.responseValues = values;
     multipleSelection.main.setAttribute('tabindex', localTabindex);
+    if (disabled) {
+      multipleSelection.disable();
+      Array.from(multipleSelection.main.getElementsByTagName('button')).map(button => button.setAttribute('type', 'button'));
+    } else {
+      multipleSelection.main.lastElementChild.onclick = listener;
+    }
     pointer.selection = multipleSelection;
   });
 
@@ -100,12 +173,16 @@ function createSelection(parent, id, callback, multiple, placeholder) {
   return pointer;
 }
 
-async function loadComponents() {
-  return components;
-}
+function createSubmitButton(parent, text) {
+  const button = appendElement(parent, 'button', {
+    id: 'submit',
+    type: 'button',
+    tabindex: tabindex,
+  });
+  tabindex++;
+  button.innerHTML = text || 'Submit';
 
-async function loadComponentStacks() {
-  return componentStacks;
+  return button;
 }
 
 createButton.onclick = function (event) {
@@ -125,22 +202,22 @@ createButton.onclick = function (event) {
   const subGrid2 = createSubGrid(mainForm, 'Components :');
 
   const selectionId = 'componentsSelection';
-  const selectionPointer = createSelection(subGrid2, selectionId, loadComponents, 'Loading...');
+  const selectionPointer = createSelection(subGrid2, selectionId, loadComponents, true, 'Loading...');
 
-  const submit = appendElement(mainForm, 'button', {
-    id: 'submit',
-    type: 'button',
-    tabindex: tabindex,
-  });
-  tabindex++;
-  submit.innerHTML = 'Submit';
-
+  const submit = createSubmitButton(mainForm);
   submit.onclick = function (event) {
-    // TODO : create stack in backend
-    console.log(selectionPointer.selection);
     const stackName = input.value;
+    if (!stackName) {
+      return;
+    }
+    const components = getSelectedItems(selectionPointer.selection);
+    if (components.length === 0) {
+      return;
+    }
+    // TODO : create stack in backend
     console.log(apiBaseUrl);
     console.log(stackName);
+    console.log(components);
   };
 };
 
@@ -151,8 +228,62 @@ editButton.onclick = function (event) {
     class: 'mainForm',
   });
 
-  const subGrid1 = createSubGrid(mainForm, 'Choose stack :');
+  const subGrid1 = createSubGrid(mainForm, 'Stack :');
+  const stackSelectionPointer = createSelection(subGrid1, 'stacksSelection', loadComponentStacks, false);
 
-  const selectionId = 'stacksSelection';
-  const selectionPointer = createSelection(subGrid1, selectionId, loadComponentStacks, false);
+  const subGrid2 = createSubGrid(mainForm, 'Components :');
+  const componentSelectionPointer = createSelection(subGrid2, 'componentsSelection', loadComponents,  true,'Choose a stack', true);
+
+  stackSelectionPointer.selection.main.lastElementChild.onclick = function (event) {
+    let selection = componentSelectionPointer.selection;
+
+    const userOptions = selection.userOptions;
+    delete userOptions.placeHolder;
+
+    const responseValues = stackSelectionPointer.selection.responseValues;
+    const stackName = getSelectedItems(stackSelectionPointer.selection)[0];
+    const selectedStack = responseValues.filter(stack => stack.name === stackName)[0];
+    selection = new vanillaSelectBox(selection.domSelector, userOptions);
+    selection.setValue(selectedStack.components.map(component => component.short));
+
+    componentSelectionPointer.selection = selection;
+  };
+
+  const submit = createSubmitButton(mainForm);
+  submit.onclick = function (event) {
+    const stackName = getSelectedItems(stackSelectionPointer.selection)[0];
+    if (stackName === undefined) {
+      return;
+    }
+    const components = getSelectedItems(componentSelectionPointer.selection);
+    if (components.length === 0) {
+      return;
+    }
+    // TODO : edit stack in backend
+    console.log(apiBaseUrl);
+    console.log(stackName);
+    console.log(components);
+  };
+};
+
+deleteButton.onclick = function (event) {
+  resetPage();
+
+  const mainForm = appendElement(document.body, 'form', {
+    class: 'mainForm',
+  });
+
+  const subGrid1 = createSubGrid(mainForm, 'Stack :');
+  const stackSelectionPointer = createSelection(subGrid1, 'stacksSelection', loadComponentStacks, false);
+
+  const submit = createSubmitButton(mainForm, 'Delete');
+  submit.onclick = function (event) {
+    const stackName = getSelectedItems(stackSelectionPointer.selection)[0];
+    if (stackName === undefined) {
+      return;
+    }
+    // TODO : edit stack in backend
+    console.log(apiBaseUrl);
+    console.log(stackName);
+  };
 };
